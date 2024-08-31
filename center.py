5f7f75ed8276a803ee6b0bad582906ee874b0d44
ajay-rs <ajayrav516@gmail.com>
2024-08-31 18:06:14 +0530

    ajay

---
+++ center.py
@@ -0,0 +1,15 @@
+txt = "banana"
+
+
+total_width = 20
+
+
+a = (total_width - len(txt)) // 2
+
+
+b= total_width - len(txt) - a
+                                                                                                   #doubt
+
+x = " " * a + txt + " " * b
+
+print(x)
